05ac153291677feee38ed4fbb2a9a3a267d0dd02
Wojtek <68703185+wojciecholejnik@users.noreply.github.com>
2021-04-10 20:30:11 +0200

    code refactor

--- Wojciech_Olejnik_Przelewy-Algorytm.js
+++ Wojciech_Olejnik_Przelewy-Algorytm.js
@@ -1,8 +1,62 @@
-// Function that returns a random integer between min and max
-function randomInt(min, max){
+// function that returns a random integer between min and max
+const randomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+// function that capitalizes interest, changes the interest rate and sends a transfer when conditions are met
+const makeTransfer = (account, highestAccount) => {
+  account.saldo -= account.balance * account.commission; // deduction of transfer commission
+  highestAccount.balance += account.balance; // transfer to a bank account with the highest interest rate
+  account.balance = 0; // deleting the account contents after a transfer
+}
+
+// function displaying the total capital every minute
+const showTotal = (accountsArray) => {
+  let totalCapital = null;
+    accountsArray.map(account => totalCapital += account.balance);
+    console.log('Total capital: ', totalCapital);
+    accountsArray.map(account => console.log('Capital at ', account.name, ': ', account.balance));
+}
+
+// function finding the bank with the currently highest interest rate
+const findHighestValue = (accountsArray) => {
+  let highestInterestRate = accountsArray[0].interestRate;
+  let account = accountsArray[0];
+  for(let i = 0; i < accountsArray.length; i++){
+    if(accountsArray[i].interestRate > highestInterestRate){
+      highestInterestRate = accountsArray[i].interestRate;
+      account = accountsArray[i];
+    }
+  }
+  return account;
+};
+
+// function that capitalizes interest, changes the interest rate and sends a transfer when conditions are met
+const makeProcess = (account, accountsArray) => {
+
+  // setting an interval for the function executing a transfer with a frequency appropriate for the selected bank
+  setInterval(() => {
+
+    account.balance += account.balance * account.interestRate; // capitalization of interest
+    account.interestRate = randomInt(1, 20)/100; //change in interest rate after capitalisation
+
+    // condition that blocks the function for an account with a balance of 0
+    if(account.balance > 0){ 
+
+      // assigning the account with the highest interest rate to a variable
+      const highestAccount = findHighestValue(accountsArray);
+
+      // condition that blocks the possibility of sending a transfer to the same bank in which the capital is located
+      if(highestAccount.name !== account.name){
+
+        // a condition that checks whether it is profitable to make a transfer
+        if((highestAccount.interestRate - account.interestRate) > account.commission){
+          makeTransfer(account, highestAccount);
+        }
+      }
+    }
+  },(account.capitalizationCycle));
 }
 
 class Account {
@@ -24,60 +78,9 @@ const account3 = new Account('Bank3');
 const accountsArray = [];
 accountsArray.push(account1, account2, account3);
 
-// function displaying the total capital every minute
-const total = () => {
-  let totalCapital = null;
-    accountsArray.map(account => totalCapital += account.balance);
-    console.log('Total capital: ', totalCapital);
-    
-    accountsArray.map(account => console.log('Capital at ', account.name, ': ', account.balance));
-}
-total(); 
-setInterval(() => {total()}, 60000);
+showTotal(accountsArray); 
+setInterval(() => {showTotal(accountsArray)}, 60000);
 
-// function finding the bank with the currently highest interest rate
-const findHighestValue = () => {
-  let highestInterestRate = accountsArray[0].interestRate;
-  let account = accountsArray[0];
-  for(let i = 0; i < accountsArray.length; i++){
-    if(accountsArray[i].interestRate > highestInterestRate){
-      highestInterestRate = accountsArray[i].interestRate;
-      account = accountsArray[i];
-    }
-  }
-  return account;
-};
-
-// function that performs transfers
-const makeTransfer = (account) => {
-
-  //setting an interval for the function executing a transfer with a frequency appropriate for the selected bank
-  setInterval(() => {
-
-    account.balance += account.balance * account.interestRate; // capitalization of interest
-    account.interestRate = randomInt(1, 20)/100; //change in interest rate after capitalisation
-
-    // condition that blocks the function for an account with a balance of 0
-    if(account.balance > 0){ 
-
-      // assigning the account with the highest interest rate to a variable
-      const highestAccount = findHighestValue();
-
-      // condition that blocks the possibility of sending a transfer to the same bank in which the capital is located
-      if(highestAccount.name !== account.name){
-
-        // a condition that checks whether it is profitable to make a transfer
-        if((highestAccount.interestRate - account.interestRate) > account.commission){
-
-          account.saldo -= account.balance * account.commission; // deduction of transfer commission
-          highestAccount.balance += account.balance; // transfer to a bank account with the highest interest rate
-          account.balance = 0; // deleting the account contents after a transfer
-        }
-      }
-    }
-  },(account.capitalizationCycle));
-}
-
-makeTransfer(account1); 
-makeTransfer(account2); 
-makeTransfer(account3);
+makeProcess(account1, accountsArray); 
+makeProcess(account2, accountsArray); 
+makeProcess(account3, accountsArray);
